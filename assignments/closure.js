8f83f799150ebdbfd8c38a3c207d75b1c5202e7b
Oluwajoba <oluwajobabello@gmail.com>
2019-08-21 03:54:12 +0100

    complete closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,11 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const worldBestCodeSchool = "Lambda";
+function worldBest() {
+  console.log(`${worldBestCodeSchool} is the best coding I ever known`)
+}
+worldBest()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
